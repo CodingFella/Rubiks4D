a6812ac148fa150d8f5ad4061fcca06830b3b670
codingfella <jacob.shalom.lin@gmail.com>
2024-06-23 15:34:03 -0700

    progress!

--- main.c
+++ main.c
@@ -19,8 +19,8 @@
 #define PI 3.14159265359
 #define FLT_MAX 3.402823466e+38F
 
-#define SCALE 0.9
-#define GAP 0.5
+#define SCALE 1.1
+#define GAP 0.6
 
 #define BG_COLOR BLACK
 
@@ -39,7 +39,8 @@ typedef struct {
 typedef struct {
     Point_3D points[NUM_CORNERS];
     uint32_t color;
-    int active;
+    int selected;
+    int id;
 } cube;
 
 typedef struct {
@@ -102,7 +103,7 @@ void generateCorners(cube *cubes, float magnitude, float camera_distance, float 
 }
 
 void resetFaces(cube *cubes, int num_cubes, const uint32_t *colors) {
-    int i, j, k;
+    int i;
     int curr_color = -1;
     for(i=0; i<num_cubes; i++) {
         if(i % 27 == 0) {
@@ -110,19 +111,24 @@ void resetFaces(cube *cubes, int num_cubes, const uint32_t *colors) {
         }
 
         cubes[i].color = colors[curr_color];
-        if(i < CUBES) {
-            cubes[i].active = 1;
-        }
-        else {
-            cubes[i].active = 0;
-        }
+        cubes[i].selected = 0;
+        cubes[i].id = i;
+
     }
+}
+
+void move_color(cube *dest, cube *src) {
+    dest->color = src->color;
 }
 
 void copy_cube(cube *dest, cube *src) {
     for(int i = 0; i < NUM_CORNERS; i++) {
         dest->points[i] = src->points[i];
     }
+
+    dest->selected = src->selected;
+    dest->id = src->id;
+
 }
 
 cube *generateCubes(cube *cubes, cube *translated_cubes, int num_cubes, float magnitude, float camera_distance, uint32_t *colors) {
@@ -133,7 +139,9 @@ cube *generateCubes(cube *cubes, cube *translated_cubes, int num_cubes, float ma
         for(j=0; j < NUM_CORNERS; j++) {
             translated_cubes[i].points[j] = cubes[i].points[j];
         }
-        translated_cubes->color = cubes->color;
+        translated_cubes[i].color = cubes[i].color;
+        translated_cubes[i].selected = cubes[i].selected;
+        translated_cubes[i].id = cubes[i].id;
     }
 
     float spacing = (float) (SCALE * 2);
@@ -169,7 +177,7 @@ cube *generateCubes(cube *cubes, cube *translated_cubes, int num_cubes, float ma
                                     x_offset + (spacing * (float) i) + offsetVectors[vector].x,
                                     y_offset + (spacing * (float) j) + offsetVectors[vector].y,
                                     z_offset + (spacing * (float) k) + offsetVectors[vector].z);
-                    translated_cubes[count].color = colors[vector];
+//                    translated_cubes[count].color = colors[vector];
                     count++;
                 }
             }
@@ -201,6 +209,8 @@ cube *generateCubes(cube *cubes, cube *translated_cubes, int num_cubes, float ma
 
         Point_3D temp;
         uint32_t temp_color;
+        int temp_selected;
+        int temp_id;
         if(min != i) {
             for(j = 0; j< NUM_CORNERS; j++) {
                 temp = translated_cubes[min].points[j];
@@ -211,6 +221,14 @@ cube *generateCubes(cube *cubes, cube *translated_cubes, int num_cubes, float ma
             temp_color = translated_cubes[min].color;
             translated_cubes[min].color = translated_cubes[i].color;
             translated_cubes[i].color = temp_color;
+
+            temp_selected = translated_cubes[min].selected;
+            translated_cubes[min].selected = translated_cubes[i].selected;
+            translated_cubes[i].selected = temp_selected;
+
+            temp_id = translated_cubes[min].id;
+            translated_cubes[min].id = translated_cubes[i].id;
+            translated_cubes[i].id = temp_id;
         }
     }
 
@@ -277,8 +295,8 @@ int pnpoly(int nvert, float *vertx, float *verty, float testx, float testy)
 {
     int i, j, c = 0;
     for (i = 0, j = nvert-1; i < nvert; j = i++) {
-        if ( ((verty[i]>testy) != (verty[j]>testy)) &&
-             (testx < (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) )
+        if ( ((verty[i]>=testy) != (verty[j]>=testy)) &&
+             (testx <= (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) )
             c = !c;
     }
     return c;
@@ -304,6 +322,7 @@ int set_hovered(cube *cube, plane *p, int x, int y) {
         verty[3] = p[i].s_pointD.y;
 
         if(pnpoly(PLANE_CORNERS, vertx, verty, (float) x, (float) y)) {
+            cube->color = HOVER_COLOR;
             return 1;
         }
     }
@@ -316,6 +335,45 @@ void set_front_hover(cube *cubes, int num_cubes) {
     for(int i=0; i<num_cubes; i++) {
 
     }
+}
+
+void draw_outline(Point_2D *points) {
+    int i;
+
+    // 01 04 02 57 76 73 15 54 46
+
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[0].x, (int) points[0].y, (int) points[1].x, (int) points[1].y, HOTPINK); // 01
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[0].x, (int) points[0].y, (int) points[4].x, (int) points[4].y, HOTPINK); // 04
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[0].x, (int) points[0].y, (int) points[2].x, (int) points[2].y, HOTPINK); // 02
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[5].x, (int) points[5].y, (int) points[7].x, (int) points[7].y, HOTPINK); // 57
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[7].x, (int) points[7].y, (int) points[6].x, (int) points[6].y, HOTPINK); // 76
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[7].x, (int) points[7].y, (int) points[3].x, (int) points[3].y, HOTPINK); // 73
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[1].x, (int) points[1].y, (int) points[5].x, (int) points[5].y, HOTPINK); // 15
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[5].x, (int) points[5].y, (int) points[4].x, (int) points[4].y, HOTPINK); // 54
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[4].x, (int) points[4].y, (int) points[6].x, (int) points[6].y, HOTPINK); // 46
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[2].x, (int) points[2].y, (int) points[6].x, (int) points[6].y, HOTPINK); // 26
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[3].x, (int) points[3].y, (int) points[2].x, (int) points[2].y, HOTPINK); // 34
+    draw_line(pixels, WIDTH, HEIGHT, (int) points[1].x, (int) points[1].y, (int) points[3].x, (int) points[3].y, HOTPINK); // 13
+}
+
+uint32_t shade(uint32_t color, int percent_change) {
+    // AABBGGRR
+    int red = (uint8_t) color;
+    int green = (uint8_t) (color >> 8);
+    int blue = (uint8_t) (color >> 16);
+    int alpha = (uint8_t) (color >> 24);
+
+    red = (100.0 + percent_change)/100.0 * (double)red;
+    blue = (100.0 + percent_change)/100.0 * (double)blue;
+    green = (100.0+percent_change)/100.0 * (double)green;
+
+    if(red > 255) red = 255;
+    if(blue > 255) blue = 255;
+    if(green > 255) green = 255;
+
+    uint32_t new_color = (uint8_t)red + ((uint8_t)green << 8) + ((uint8_t)blue << 16) + ((uint8_t)alpha << 24);
+
+    return new_color;
 }
 
 
@@ -341,14 +399,23 @@ void draw_cube(uint32_t *canvas, int width, int height, cube *curr_cube, Point_3
 
     plane planes[NUM_PLANES] = {p0123, p5140, p4062, p5476, p5173, p7362};
 
-    if(mouseX != -1 && mouseY != -1 && !found_hovered) set_hovered(curr_cube, planes, mouseX, mouseY);
+//    if(mouseX != -1 && mouseY != -1) set_hovered(curr_cube, planes, mouseX, mouseY);
 
 
-    for(int i=0; i<NUM_PLANES; i++) {
-        if(i%3==0) planes[i].color = curr_cube->color;
-        if(i%3==1) planes[i].color = ((curr_cube->color & 0x00EEEEEE) >> 1) | 0xFF000000;
-        if(i%3==2) planes[i].color = ((curr_cube->color & 0x007E7E7E) >> 1) | 0xFF000000;
+    for (int i = 0; i < NUM_PLANES; i++) {
+        if (i % 3 == 0) planes[i].color = curr_cube->color;
+        if (i % 3 == 1) planes[i].color = ((curr_cube->color & 0x00EEEEEE) >> 1) | 0xFF000000;
+        if (i % 3 == 2) planes[i].color = ((curr_cube->color & 0x009E9E9E) >> 1) | 0xFF000000;
     }
+    if(curr_cube->selected) {
+//        curr_cube->color = HOVER_COLOR;
+        for(int i=0; i<NUM_PLANES; i++) {
+            planes[i].color = shade(planes[i].color, 1000);
+        }
+    }
+
+
+
 
     draw_planes(canvas, width, height, planes, NUM_PLANES);
 }
@@ -383,10 +450,162 @@ void process_key(int input) {
 
 }
 
+void change_center(cube *cubes, int *cube_order) {
+    // brute force first one, and try to optimize for rest
+    int i;
+    cube temp_cubes[CUBES];
+    int count = 0;
+
+
+    for(i = cube_order[0] * CUBES; i < (cube_order[0] + 1) * CUBES; i++) {
+        move_color(&temp_cubes[count], &cubes[i]);
+        count++;
+    }
+
+    count = cube_order[0] * CUBES;
+    for(i = cube_order[1] * CUBES; i < (cube_order[1] + 1) * CUBES; i++) {
+        move_color(&cubes[count], &cubes[i]);
+        count++;
+    }
+
+    count = cube_order[1] * CUBES;
+    for(i = cube_order[2] * CUBES; i < (cube_order[2] + 1) * CUBES; i++) {
+        move_color(&cubes[count], &cubes[i]);
+        count++;
+    }
+
+    count = cube_order[2] * CUBES;
+    for(i = cube_order[3] * CUBES; i < (cube_order[3] + 1) * CUBES; i++) {
+        move_color(&cubes[count], &cubes[i]);
+        count++;
+    }
+
+    count = 0;
+    for(i = cube_order[3] * CUBES; i < (cube_order[3] + 1) * CUBES; i++) {
+        move_color(&cubes[i], &temp_cubes[count]);
+        count++;
+    }
+
+}
+
+void rotate_edge(cube *cubes, int select) {
+
+}
+
+void rotate_center(cube *cubes, int select, int order) {
+
+    // order is 4 digit: 3210
+
+    int side_1 = 1;
+    int side_2 = 4;
+    int side_3 = 6;
+    int side_4 = 2;
+
+    int i, j, count;
+    cube temp_cubes[9];
+
+    int anchor_1[4] = {1 * CUBES + 8, 1 * CUBES + 7, 1 * CUBES + 6, 9};
+    int anchor_2[4] = {2 * CUBES + 6, 2 * CUBES + 3, 2 * CUBES + 0, 9};
+
+//    int anchor_3;
+    int anchor_3[4] = {3 * CUBES + 8, 3 * CUBES + 7, 3 * CUBES + 6, -3};
+
+
+    int anchor_4[4] = {4 * CUBES + 2, 4 * CUBES + 5, 4 * CUBES + 8, 9};
+
+    // int anchor_5;
+    int anchor_5[4] = {5 * CUBES + 24, 5 * CUBES + 25, 5 * CUBES + 26, -3};
+
+    int anchor_6[4] = {6 * CUBES + 0, 6 * CUBES + 1, 6 * CUBES + 2, 9};
+
+    int *anchors[7] = {NULL, anchor_1, anchor_2, anchor_3, anchor_4, anchor_5, anchor_6};
 
 
 
-uint32_t *render(int keyboard_input, float a, float b, float c, int x, int y) {
+    count = 0;
+
+    for(i = 0; i < DIMENSION; i++) {
+        for(j = 0; j < DIMENSION; j++) {
+            move_color(&temp_cubes[count], &cubes[anchors[side_1][i] + anchors[side_1][3] * j]);
+            count++;
+        }
+    }
+
+
+    for(i = 0; i < DIMENSION; i++) {
+        for(j = 0; j < DIMENSION; j++) {
+            move_color(&cubes[anchors[side_1][i] + anchors[side_1][3] * j], &cubes[anchors[side_2][i] + anchors[side_2][3] * j]);
+        }
+    }
+
+
+    for(i = 0; i < DIMENSION; i++) {
+        for(j = 0; j < DIMENSION; j++) {
+            move_color(&cubes[anchors[side_2][i] + anchors[side_2][3] * j], &cubes[anchors[side_3][i] + anchors[side_3][3] * j]);
+        }
+    }
+
+    for(i = 0; i < DIMENSION; i++) {
+        for(j = 0; j < DIMENSION; j++) {
+            move_color(&cubes[anchors[side_3][i] + anchors[side_3][3] * j], &cubes[anchors[side_4][i] + anchors[side_4][3] * j]);
+        }
+    }
+
+    count = 0;
+    for(i = 0; i < DIMENSION; i++) {
+        for(j = 0; j < DIMENSION; j++) {
+            move_color(&cubes[anchors[side_4][i] + anchors[side_4][3] * j], &temp_cubes[count]);
+            count++;
+        }
+    }
+}
+
+void rotate_corner(cube *cubes, int select) {
+
+}
+
+
+
+void rotate(cube *cubes, int select) {
+    // 3 types: corner, edge, center. and core
+
+    // core
+    if(select == 13) {
+        return;
+    }
+
+    if(select % 2 == 1) {
+        rotate_edge(cubes, select);
+    }
+    else if(select == 4 || select == 10 || select == 12 || select == 14 || select == 16 || select == 22) {
+        int order;
+        switch(select) {
+            case 4:
+                order = 1462;
+            case 10:
+                order = 4325;
+            case 12:
+                order = 6315;
+            case 14:
+                order = 5136;
+            case 16:
+                order = 5234;
+            case 22:
+                order = 2641;
+            default:
+                order = 0;
+        }
+        rotate_center(cubes, select, order);
+    }
+    else {
+        rotate_corner(cubes, select);
+    }
+}
+
+
+
+
+uint32_t *render(int dt, int keyboard_input, float a, float b, float c, int x, int y, int select, int to_rotate) {
 
 //    process_key(keyboard_input);
     found_hovered = 0;
@@ -404,15 +623,74 @@ uint32_t *render(int keyboard_input, float a, float b, float c, int x, int y) {
 
 
 
-    uint32_t colors[SIDES] = {PURPLE, WHITE, ORANGE, BLUE, RED, GREEN, YELLOW, LILAC};
+    uint32_t colors[SIDES] = {PURPLE, WHITE, ORANGE, BLUE, RED, GREEN, YELLOW, PINK};
 
-    if(a == 0 && b == 0 && c == 0) {
+    if(dt == 0) {
         resetFaces(cubes, num_cubes, colors);
     }
 
-    for(int i=0; i<num_cubes; i++) {
-//        printf("%d: %b\n", i, cubes[i].color);
+    if(to_rotate) {
+        if(select >= CUBES) {
+            int cube_order[4];
+            switch(select/CUBES) {
+                case 1:
+                    cube_order[0] = 1;
+                    cube_order[1] = 7;
+                    cube_order[2] = 6;
+                    cube_order[3] = 0;
+                    break;
+                case 2:
+                    cube_order[0] = 2;
+                    cube_order[1] = 7;
+                    cube_order[2] = 4;
+                    cube_order[3] = 0;
+                    break;
+                case 3:
+                    cube_order[0] = 3;
+                    cube_order[1] = 7;
+                    cube_order[2] = 5;
+                    cube_order[3] = 0;
+                    break;
+                case 4:
+                    cube_order[0] = 4;
+                    cube_order[1] = 7;
+                    cube_order[2] = 2;
+                    cube_order[3] = 0;
+                    break;
+                case 5:
+                    cube_order[0] = 5;
+                    cube_order[1] = 7;
+                    cube_order[2] = 3;
+                    cube_order[3] = 0;
+                    break;
+                case 6:
+                    cube_order[0] = 6;
+                    cube_order[1] = 7;
+                    cube_order[2] = 1;
+                    cube_order[3] = 0;
+                    break;
+                default:
+                    break;
+            }
+            change_center(cubes, cube_order);
+        }
+        else {
+            rotate(cubes, select);
+
+        }
     }
+
+    if(select >= 0 && select < num_cubes) {
+        for(int i=0; i<num_cubes; i++) {
+            if(cubes[i].id == select) {
+                cubes[select].selected = 1;
+            }
+            else {
+                cubes[i].selected = 0;
+            }
+        }
+    }
+
 
     fill_screen(pixels, WIDTH, HEIGHT, BG_COLOR);
 
@@ -428,6 +706,16 @@ uint32_t *render(int keyboard_input, float a, float b, float c, int x, int y) {
         draw_cube(pixels, WIDTH, HEIGHT, &translated_cubes[i], translated_cubes[i].points, translated_cubes[i].color, camera, x, y, i);
     }
 
+
+    for(int i=0; i< num_cubes; i++) {
+        if(translated_cubes[i].selected == 1){
+            Point_2D c_2D[NUM_CORNERS];
+            convert_3D_to_2D(c_2D, WIDTH, HEIGHT, translated_cubes[i].points, camera);
+
+            draw_outline(c_2D);
+        }
+    }
+
     return pixels;
 }
 
@@ -436,7 +724,7 @@ uint32_t *render(int keyboard_input, float a, float b, float c, int x, int y) {
 
 int main(void) {
 //    printf("hello world");
-    render(0, 0, 0, 0, 397, 301);
+    render(0, 0, 1, 0, 0, 397, 301, 4, 1);
 }
 
 void *memcpy(void *dest, const void *src, size_t n) {
--- script.js
+++ script.js
@@ -12,6 +12,13 @@ let C = 0;
 let x = 0;
 let y = 0;
 
+let dt = 0;
+
+let current_cube = 0;
+let current_face = 0;
+
+let to_rotate = 0;
+
 let lastCall = 0;
 const delay = 20;
 
@@ -25,25 +32,25 @@ async function startDemo() {
 
     render(instance);
 
-    document.getElementById("app").onclick = function(e) {
-        // e = Mouse click event.
-        let rect = e.target.getBoundingClientRect();
-        let mouse_x = e.clientX - rect.left; //x position within the element.
-        let mouse_y = e.clientY - rect.top;  //y position within the element.
-        x = mouse_x;
-        y = mouse_y;
-        console.log("x : " + x + " ; y : " + y + ".");
-
-        // const now = new Date().getTime();
-        // if (now - lastCall < delay) {
-        //     return;
-        // }
-        // lastCall = now;
-
-        render(instance);
-        x = -1;
-        y = -1;
-    }
+    // document.getElementById("app").onclick = function(e) {
+    //     // e = Mouse click event.
+    //     let rect = e.target.getBoundingClientRect();
+    //     let mouse_x = e.clientX - rect.left; //x position within the element.
+    //     let mouse_y = e.clientY - rect.top;  //y position within the element.
+    //     x = mouse_x;
+    //     y = mouse_y;
+    //     console.log("x : " + x + " ; y : " + y + ".");
+    //
+    //     // const now = new Date().getTime();
+    //     // if (now - lastCall < delay) {
+    //     //     return;
+    //     // }
+    //     // lastCall = now;
+    //
+    //     render(instance);
+    //     x = -1;
+    //     y = -1;
+    // }
 
     document.addEventListener("keydown", async(event) => {
         if(event.key === 'w') {
@@ -64,18 +71,57 @@ async function startDemo() {
         } else if(event.key === 'k') {
             current_input = 6;
             C -= magnitude;
+        } else if(event.key === '.') {
+            current_cube += 1;
+            if(current_cube > 26) {
+                current_cube = 26;
+            }
+        } else if(event.key === ',') {
+            current_cube -= 1;
+            if(current_cube < 0) {
+                current_cube = 0;
+            }
+        } else if (event.key === '1') {
+            current_face = 0;
+            current_cube = 0;
+        } else if (event.key === '2') {
+            current_face = 1;
+            current_cube = 0;
+        } else if (event.key === '3') {
+            current_face = 2;
+            current_cube = 0;
+        } else if (event.key === '4') {
+            current_face = 3;
+            current_cube = 0;
+        } else if (event.key === '5') {
+            current_face = 4;
+            current_cube = 0;
+        } else if (event.key === '6') {
+            current_face = 5;
+            current_cube = 0;
+        } else if (event.key === '7') {
+            current_face = 6;
+            current_cube = 0;
+        } else if (event.key === '8') {
+            current_face = 7;
+            current_cube = 0;
+        } else if (event.key === 'r') {
+            to_rotate = 1;
+            console.log("r");
         }
-        console.log(current_input);
+        // console.log(current_input);
         render(instance);
     })
 }
 
 function render(instance) {
-    const pixels = instance.exports.render(current_input, A, B, C, x, y);
+    const pixels = instance.exports.render(dt, current_input, A, B, C, x, y, current_face * 27 + current_cube, to_rotate);
     const buffer = instance.exports.memory.buffer;
     const imageData = new ImageData(new Uint8ClampedArray(buffer, pixels, app.width * app.height * 4), app.width);
     ctx.putImageData(imageData, 0, 0);
     current_input = 0;
+    dt += 1;
+    to_rotate = 0;
 }
 
 startDemo();
